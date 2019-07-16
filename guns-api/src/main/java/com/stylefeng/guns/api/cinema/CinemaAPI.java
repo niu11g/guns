f67d436bee5fb662354f6e4b7db398a3ba6298ca
niu11g <2633340019@qq.com>
2019-07-16 22:07:25 +0800

    增加影院API

--- guns-api/src/main/java/com/stylefeng/guns/api/cinema/CinemaAPI.java
+++ guns-api/src/main/java/com/stylefeng/guns/api/cinema/CinemaAPI.java
@@ -1,10 +1,26 @@
 package com.stylefeng.guns.api.cinema;
 
 import com.stylefeng.guns.api.cinema.vo.CinemaRequestVO;
-import java.util.List;
+import com.stylefeng.guns.api.cinema.vo.CinemaVO;
+
 
 public interface CinemaAPI {
+    //根据CinemaQueryVO,查询影院列表
+    Page<CinemaVO> getCinemas(CinemaRequestVO cinemaRequestVO);
+    //根据条件获取品牌列表
 
-    List<CinemaRequestVO> getCinemas(int brandId, int hallType, int districtId, int pageSize, int nowPage);
+    //获取行政区域列表
+
+    //获取影厅类型列表
+
+    //根据影院编号，获取影院信息
+
+    //获取所有电影的信息和对应的放映场次信息，根据影院编号
+
+    //根据影院编号获取影院信息
+
+    //根据放映场次获取放映信息
+
+    //根据放映场次查询播放的电影编号，然后根据电影编号获取对应的电影信息
 
 }
--- guns-gateway/src/main/java/com/stylefeng/guns/rest/GateWayApplication.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/GateWayApplication.java
@@ -4,12 +4,23 @@ import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
 @EnableDubboConfiguration
 public class GateWayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class, args);
+//        String str = "成都市(成华区)(武侯区)(高新区)";
+//        Pattern p = Pattern.compile(".*?(?=\\()");
+//        Matcher m = p.matcher(str);
+//        if (m.find()) {
+//            System.out.println(m.group());
+//        }
     }
 
 }
+
+
