627bd7e1afb861717bab68f2d766ce626046733d
niu11g <2633340019@qq.com>
2019-07-23 21:02:27 +0800

    190723

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/cinema/CinemaController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/cinema/CinemaController.java
@@ -6,26 +6,36 @@ import com.stylefeng.guns.api.cinema.CinemaAPI;
 import com.stylefeng.guns.api.cinema.vo.CinemaRequestVO;
 import com.stylefeng.guns.api.cinema.vo.CinemaVO;
 import com.stylefeng.guns.rest.modular.vo.ResponseVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/cinema/")
 public class CinemaController {
 
-    @Reference(interfaceClass = CinemaAPI.class)
+    @Reference(interfaceClass = CinemaAPI.class,check=false)
     private CinemaAPI cinemaAPI;
 
-    @RequestMapping(value="getCinemas",method = RequestMethod.GET)
+    @RequestMapping(value="getCinemas")
     //查询影院列表
     public ResponseVO getCinemas(CinemaRequestVO cinemaVO){
-        //按照五个条件进行筛选
-        Page<CinemaVO> cinemas = cinemaAPI.getCinemas(cinemaVO);
-        //判断是否有满足条件的影院
-
-
-        return null;
+        try {
+            //按照五个条件进行筛选
+            Page<CinemaVO> cinemas = cinemaAPI.getCinemas(cinemaVO);
+            //判断是否有满足条件的影院
+            if(cinemas.getRecords() == null || cinemas.getRecords().size() == 0 ){
+                return ResponseVO.success("没有影院信息");
+            }else{
+                return ResponseVO.success(cinemas.getCurrent(),(int)cinemas.getPages(),"",cinemas.getRecords());
+            }
+        }catch (Exception e){
+            //如果出现异常，应该如何处理
+            log.error("获取影院列表异常",e);
+            return ResponseVO.serviceFail("查询影院列表失败");
+        }
     }
     //获取影院列表
     @RequestMapping(value="getCondition",method=RequestMethod.GET)
