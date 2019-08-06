b8200fa89469d4cf649c890a4d87e0f35f6ca8e8
niu11g <2633340019@qq.com>
2019-08-06 15:58:13 +0800

    190806

--- guns-cinema/src/main/java/com/stylefeng/guns/rest/CinemaApplication.java
+++ guns-cinema/src/main/java/com/stylefeng/guns/rest/CinemaApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
+
+
 public class CinemaApplication {
 
     public static void main(String[] args) {
