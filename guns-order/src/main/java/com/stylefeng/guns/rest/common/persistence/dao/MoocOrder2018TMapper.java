331fed04574a2b0e6d3a44a61d4b8cbe70ac79f4
niu11g <2633340019@qq.com>
2019-08-11 11:53:23 +0800

    190811订单模块创建与实体生成

---
+++ guns-order/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocOrder2018TMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocOrder2018T;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 订单信息表 Mapper 接口
+ * </p>
+ *
+ * @author ly
+ * @since 2019-08-11
+ */
+public interface MoocOrder2018TMapper extends BaseMapper<MoocOrder2018T> {
+
+}
