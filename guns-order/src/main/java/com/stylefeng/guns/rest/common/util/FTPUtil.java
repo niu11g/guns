e6b54d34388efa8b70cda99540d12551d44ba67a
niu11g <2633340019@qq.com>
2019-08-14 22:07:34 +0800

    190814连接FTPSpringBoot配置

--- guns-order/src/main/java/com/stylefeng/guns/rest/common/util/FTPUtil.java
+++ guns-order/src/main/java/com/stylefeng/guns/rest/common/util/FTPUtil.java
@@ -1,20 +1,26 @@
 package com.stylefeng.guns.rest.common.util;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.net.ftp.FTPClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 @Slf4j
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "ftp")
 public class FTPUtil {
 
     //地址 端口 用户名 密码
-    private String hostName = "192.168.0.101";
-    private Integer port = 2100;
-    private String username = "ftp";
-    private String password = "ftp";
+    private String hostName;
+    private Integer port;
+    private String userName;
+    private String password;
 
     private FTPClient ftpClient = null;
 
@@ -23,7 +29,7 @@ public class FTPUtil {
             ftpClient = new FTPClient();
             ftpClient.setControlEncoding("utf-8");
             ftpClient.connect(hostName,port);
-            ftpClient.login(username,password);
+            ftpClient.login(userName,password);
         }catch (Exception e){
             log.error("初始化FTP失败",e);
         }
@@ -61,8 +67,7 @@ public class FTPUtil {
 
     public static void main(String[] args){
         FTPUtil ftpUtil = new FTPUtil();
-        String fileStrByAddress = ftpUtil.getFileStrByAddress("seats/cgs.json");
-        System.out.println(fileStrByAddress);
+
     }
 
 }
