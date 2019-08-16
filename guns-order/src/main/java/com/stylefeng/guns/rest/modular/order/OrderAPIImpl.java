4b78d1cbc0e2be96b5b774d75f66c1a77e60b0e8
niu11g <2633340019@qq.com>
2019-08-17 00:28:47 +0800

    190817判断选购的座位是否已售出并且是真实存在的座位信息

--- guns-order/src/main/java/com/stylefeng/guns/rest/modular/order/OrderAPIImpl.java
+++ guns-order/src/main/java/com/stylefeng/guns/rest/modular/order/OrderAPIImpl.java
@@ -1,9 +1,13 @@
 package com.stylefeng.guns.rest.modular.order;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.stylefeng.guns.api.order.OrderAPI;
 import com.stylefeng.guns.api.order.vo.OrderInfoVO;
 import com.stylefeng.guns.rest.common.persistence.dao.MoocOrderTMapper;
+import com.stylefeng.guns.rest.common.persistence.model.MoocOrderT;
+import com.stylefeng.guns.rest.common.util.FTPUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.util.List;
@@ -15,19 +19,58 @@ public class OrderAPIImpl implements OrderAPI {
     @Autowired
     private MoocOrderTMapper moocOrderTMapper;
 
+    @Autowired
+    private FTPUtil ftpUtil;
+
     //验证售出的票是否为真
     @Override
     public boolean isTrueSeats(String fieldId, String seats) {
         //根据FieldId找到对应的座位位置图
         String seatPath = moocOrderTMapper.getSeatsByFieldId(fieldId);
         //读取位置图，判断seats是否为真
-
-        return false;
+        String fileStrByAddress = ftpUtil.getFileStrByAddress(seatPath);
+        //将fileStrByAddress转换为JSON对象
+        JSONObject jsonObject = JSONObject.parseObject(fileStrByAddress);
+        //seats=1,2,3  ids="1,3,4,5,6,7,88"
+        String ids = jsonObject.get("ids").toString();
+        //每一次匹配上的，都给isTrue+1
+        String[] seatArrs = seats.split(",");
+        String[] idArrs = ids.split(",");
+        int isTrue = 0;
+        for(String id : idArrs){
+            for(String seat : seatArrs){
+                if(seat.equalsIgnoreCase(id)){
+                    isTrue++;
+                }
+            }
+        }
+        //如果匹配上的数量与已售座位数一致，则表示全部匹配上了
+        if(seatArrs.length == isTrue){
+            return true;
+        }else{
+            return false;
+        }
     }
     //已经销售的座位里，有没有这些座位
     @Override
     public boolean isNotSoldSeats(String fieldId, String seats) {
-        return false;
+        EntityWrapper entityWrapper = new EntityWrapper();
+        entityWrapper.eq("field_id",fieldId);
+
+        List<MoocOrderT> list = moocOrderTMapper.selectList(entityWrapper);
+        String[] seatArrs = seats.split(",");
+
+        for(MoocOrderT moocOrderT : list){
+            String[] ids = moocOrderT.getSeatsIds().split(",");
+            for(String id : ids){
+                for(String seat : seatArrs){
+                    if(id.equalsIgnoreCase(seat)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
     //创建订单信息
     @Override
